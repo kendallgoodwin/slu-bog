4a694c510a4282f8008c2204b449886f41f3a8bb
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-04 16:27:13 -0700

    basic setup with routes

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,23 +1,39 @@
 class CreaturesController < ApplicationController
   def index
-    @creature = Creature.all
+    @creatures = Creature.all
   end
 
   def create
+    Creature.create creature_params
+    redirect_to creatures_path
   end
 
   def new
+    @creature = Creature.new
   end
 
   def edit
+    @creature = Creature.find params[:id]
   end
 
   def show
+    @creature = Creature.find params[:id]
   end
 
   def update
+    c = Creature.find params[:id]
+    c.update creature_params
+    redirect_to creatures_path
   end
 
   def destroy
+    Creature.find(params[:id]).delete
+    redirect_to creatures_path
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 end
