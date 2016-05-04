269376d63752f88a353975f6e7c3e9a0ce677e60
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-04 16:08:54 -0700

    initial commit

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,23 @@
+class CreaturesController < ApplicationController
+  def index
+    @creature = Creature.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/creature_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CreatureControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
